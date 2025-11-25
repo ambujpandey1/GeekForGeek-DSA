15e03750997da50ad53ee46c6dd5539c1090eb40
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-11-25 18:27:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Asteroid Collision/asteroid-collision.java
@@ -0,0 +1,32 @@
+// User function Template for Java
+
+class Solution {
+    public static int[] asteroidCollision(int N, int[] asteroids) {
+        int n=N;
+        ArrayList<Integer> st=new ArrayList<>(); // use as a stack
+
+        for(int i=0;i<n;i++){
+
+            if(asteroids[i]>0) st.add(asteroids[i]);
+
+            else{
+
+                while(!st.isEmpty() && st.get(st.size()-1)>0 && st.get(st.size()-1)<Math.abs(asteroids[i])){
+                    st.remove(st.size()-1);
+                }
+
+                if(!st.isEmpty() &&st.get(st.size()-1)==Math.abs(asteroids[i]) )  st.remove(st.size()-1);
+
+                else if(st.isEmpty() || st.get(st.size()-1)<0) st.add(asteroids[i]);
+            }
+        }
+
+        int[] ans=new int[st.size()];
+        for(int i=0;i<st.size();i++){
+            ans[i]=st.get(i);
+        }
+
+        return ans;
+        
+    }
+}
