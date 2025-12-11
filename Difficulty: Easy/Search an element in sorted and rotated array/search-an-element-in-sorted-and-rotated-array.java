06df0b84895825be4dd0ad72657cb31436de3fd2
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-12-11 23:55:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search an element in sorted and rotated array/search-an-element-in-sorted-and-rotated-array.java
@@ -0,0 +1,27 @@
+// User function Template for Java
+
+class Solution {
+    static int Search(int nums[], int target) {
+        int l=0;
+        int h=nums.length-1;
+        int mid=l+(h-l)/2;
+
+        while(l<=h){
+            mid=l+(h-l)/2;
+            if(nums[mid]==target) return mid;
+           
+           // left portion sorted
+            if(nums[l]<=nums[mid]){
+                if(nums[l] <= target && target < nums[mid]) h=mid-1;
+                else l=mid+1;
+            }
+            // right portion sorted
+            else{
+               if(nums[mid] <= target && target <= nums[h]) l=mid+1;
+                else h=mid-1; 
+            }
+        }
+        return -1;
+        
+    }
+}
