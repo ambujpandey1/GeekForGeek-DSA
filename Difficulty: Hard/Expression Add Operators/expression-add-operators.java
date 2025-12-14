2b7fb115f77417f73f6c793ac54196c030350ea6
Ambuj Kumar pandey <129196303+ambujpandey1@users.noreply.github.com>
2025-12-14 19:28:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Expression Add Operators/expression-add-operators.java
@@ -0,0 +1,30 @@
+class Solution {
+    private static void solve(int index,String num,int target, String curr,List<String> ans, long prev, long res){
+        if(index==num.length()){
+            if(res==target) ans.add(curr);
+            return;
+        }
+
+        String st="";
+        long currRes=0;
+        for(int i=index;i<num.length();i++){
+            if(i>index && num.charAt(index)=='0') break;
+            st+=num.charAt(i);
+            currRes=currRes*10+(num.charAt(i)-'0');
+            if(index==0){
+                solve(i+1,num,target,st,ans,currRes,currRes);
+            }
+            else{
+                solve(i+1,num,target,curr+"+"+st,ans,currRes,res+currRes);
+                solve(i+1,num,target,curr+"-"+st,ans,-currRes,res-currRes);
+                solve(i+1,num,target,curr+"*"+st,ans,prev*currRes,res-prev+(prev*currRes));
+            }
+        }
+    }
+    public ArrayList<String> findExpr(String num, int target) {
+       ArrayList<String> ans=new ArrayList<>();
+        solve(0,num,target,"",ans,0,0);
+        return ans;
+        
+    }
+}
